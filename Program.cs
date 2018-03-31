d59548ab4f8e5b2e5a258fd76b787eaace27018e
Marvin Hinkley <hinkley.marvin@gmail.com>
2018-03-31 08:55:47 -0700

    working .net client for go service

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Person;
+using Grpc.Core;
+using static Person.PersonService;
+
+namespace net_grpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "127.0.0.1:3000";
+            var channel = new Channel(host, ChannelCredentials.Insecure);
+
+            try
+            {
+                var client = new PersonServiceClient(channel);
+
+                var message = client.Speak(new Person.Void());
+                Console.WriteLine(message.Value);
+
+                client.DyeHair(new Person.ColorMessage{Color = "Green"});
+                message = client.Speak(new Person.Void());
+                Console.WriteLine(message.Value);
+            }
+            finally
+            {
+                channel.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
